1e10b19575a2e2248c40d31604467394a07b79ce
Terry <terryvilver@gmail.com>
2019-10-01 02:05:18 +0200

    Addition de temps

--- tp1/Temps.java
+++ tp1/Temps.java
@@ -2,24 +2,42 @@
  * hh/mm/ss et affiche comme résultat leur somme.
  */
 import java.util.Scanner;
+
+
  
 public class Temps{
 	
 	public static void main (String []args){
 		Scanner scan = new Scanner(System.in);
 
-		int hh[] = new int[2];
-		int mm[] = new int[2];
-		int ss[] = new int[2];
-		
+		int hh[] = new int[3];
+		int mm[] = new int[3];
+		int ss[] = new int[3];
+
+		int retenuMin, retenuSec;
+
 		for(int i = 0; i < hh.length; i++)
 		{
-			System.out.println("Entrez l'heure : ");
-			hh[i] = scan.nextInt();
-			System.out.println("Entrez les minutes : ");
-			mm[i] = scan.nextInt();
-			System.out.println("Entrez les secondes: ");
-			ss[i] = scan.nextInt();
-		}	
+			if(i < 2)
+			{
+				System.out.print("Entrez l'heure ( numéro " + (i+1) + " ) : ");
+				hh[i] = scan.nextInt();
+				System.out.print("Entrez les minutes ( numéro " + (i+1) + " ) : ");
+				mm[i] = scan.nextInt();
+				System.out.print("Entrez les secondes ( numéro " + (i+1) + " ) : ");
+				ss[i] = scan.nextInt();
+			}
+			else
+			{
+				retenuSec = ( ss[0] + ss[1] ) / 60;
+				ss[2] = ( ss[0] + ss[1] ) - ( retenuSec * 60 );
+
+				retenuMin = ( mm[0] + mm[1] ) / 60;
+				mm[2] = ( mm[0] + mm[1] + retenuSec ) - ( retenuMin * 60 );
+
+				hh[2] = ( hh[0] + hh[1] + retenuMin );
+			}
+		}
+		System.out.println("Temps additionné : " + hh[2] + "/" + mm[2] + "/" + ss[2]);
 	}
 }
