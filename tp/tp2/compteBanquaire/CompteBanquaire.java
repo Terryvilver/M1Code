b7029cb47c7605105aaf7970d241aeb1b466e3bb
Terry <terryvilver@gmail.com>
2019-10-11 00:00:38 +0200

    Fin du programme compte bancaire

--- tp/tp2/compteBanquaire/CompteBanquaire.java
+++ tp/tp2/compteBanquaire/CompteBanquaire.java
@@ -5,7 +5,7 @@ public class CompteBanquaire {
     private int jour;
     private int mois;
     private int annee;
-    private int solde;
+    private int montant;
     private static int nb_de_comptes_crees_ = 0;
 
     CompteBanquaire() {
@@ -13,25 +13,66 @@ public class CompteBanquaire {
             jour = 0;
             mois = 0;
             annee = 0;
-            solde = 0;
+            montant = 0;
             nb_de_comptes_crees_++;
         }
 
-    CompteBanquaire(String nom, int jour, int mois, int annee, int solde) {
+    CompteBanquaire(String nom, int jour, int mois, int annee, int montant) {
         this.nom = nom;
         this.jour = jour;
         this.mois = mois;
         this.annee = annee;
-        this.solde = solde;
+        this.montant = montant;
         nb_de_comptes_crees_++;
     }
 
+    /* Getters */
+    public int getMontant() {
+        return this.montant;
+    }
+
+    public String getDateOuverture() {
+        return this.jour + "/" + this.mois + "/" + this.annee;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public int getNbComptesCrees() {
+        return this.nb_de_comptes_crees_;
+    }
+
+    /* Setters */
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setDateOuverture(int jour, int mois, int annee) {
+        this.jour = jour;
+        this.mois = mois;
+        this.annee = annee;
+    }
+
+    public void setMontant(int montant) {
+        this.montant = montant;
+    }
+
+    /*toString */
+
     public String toString() {
-        return this.nom + ";ouvert le " + this.jour + "/" + this.mois + "/" + this.annee + ";solde " + this.solde;
+        return this.nom + ";ouvert le " + this.jour + "/" + this.mois + "/" + this.annee + ";solde " + this.montant + "€";
     }
 
     public static void main(String []args) {
         CompteBanquaire a = new CompteBanquaire("jlb",12,5,2013,1000);
         System.out.println(a);
+
+        CompteBanquaire stud = new CompteBanquaire ();
+        stud.setNom("marcel"); stud.setDateOuverture(2,3,2013); stud.setMontant(1000);
+        System.out.println(stud.getNom());
+        System.out.println(a.getDateOuverture());
+        System.out.println(a.getMontant());
+        System.out.println("nb_de_comptes_crées_:" + a.getNbComptesCrees());
     }
 }
