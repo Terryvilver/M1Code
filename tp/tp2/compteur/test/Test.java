4fb61af5900f9c0a4a48b5033c927d9497684018
Terry <terryvilver@gmail.com>
2019-10-15 20:40:45 +0200

    test des methodes en cascade

--- tp/tp2/compteur/test/Test.java
+++ tp/tp2/compteur/test/Test.java
@@ -3,13 +3,13 @@ import comptage.Compteur;
 
 public class Test {   
     public static void main(String []args) {
-        Compteur c1 = new Compteur();
+        comptage.Compteur c1 = new Compteur();
         System.out.print(c1 + " ");
 
-        c1.incrementation(10);
+        c1.incrementation(80); 
         System.out.print(c1 + " ");
 
-        c1.decrementation(20);
+        c1.decrementation(20).decrementation(20).decrementation(20); //Utilisation de methode en cascade
         System.out.print(c1 + " ");
     }
 }
