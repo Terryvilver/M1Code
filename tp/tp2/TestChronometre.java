b41fd3c685cb4212f15a0e51da694e364c34eee7
Terry <terryvilver@gmail.com>
2019-10-20 19:50:19 +0200

    ajout d'une fonction arrêt du chronometre

--- tp/tp2/TestChronometre.java
+++ tp/tp2/TestChronometre.java
@@ -1,11 +1,11 @@
 import java.util.*;
 
-
 public class TestChronometre {
     public static void main(String []args) {
 		Scanner scan = new Scanner(System.in);
 		Chronometre c1 = new Chronometre();
-		boolean lancement = false;
+		boolean lancement = false; 
+		String arret; char arret1;
 		int test = 0;
 
 		System.out.println("Voulez-vous lancez le chronometre ?(1:oui ou 2:non)");
@@ -19,9 +19,13 @@ public class TestChronometre {
 					Thread.sleep(1000);
 					c1.incrementation();
 					System.out.println(c1.getHeure() + ":" + c1.getMinute() + ":" + c1.getSeconde());
+
+					//Arrête le chronometre
+					arret = /*scan.nextLine()*/"a";
+					arret1  = arret.charAt(0);
 				}
 			}catch(InterruptedException e) {
-				System.out.println("interrupted.");
+
 			}
 		}
     }
