9727bb52526670df92843842f37375001463dd79
Terry <terryvilver@gmail.com>
2019-10-29 23:46:47 +0100

    Creation compte a rebours part 1

---
+++ tp/tp2/CompteRebours.java
@@ -0,0 +1,13 @@
+//Le but est de créer un compte a rebours
+
+public class CompteRebours extends Compteur {
+    private int heure;
+    private int minute;
+    private int seconde;
+
+    CompteRebours() {
+        super();
+    }
+
+
+}
