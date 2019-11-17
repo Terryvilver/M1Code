8103e4a94b7ff16927ead65d1633e96f5c6b1053
Terry <terryvilver@gmail.com>
2019-11-17 23:39:01 +0100

    modification en mode programmation orienté objet

--- tp/projet/Jeu.java
+++ tp/projet/Jeu.java
@@ -8,22 +8,28 @@ public class Jeu {
 	public int nb_essai;
 	public char lettre;
 	public File dico_court;
-	public BufferedReader reader;
+	public File dico_long;
 	public ArrayList<String> mots;
-	
-	public static void main(String []args) throws IOException {
-		File dico_court = new File("monDicoCourt.txt");
+
+	Jeu() {
+		nb_essai = 20;
+		lettre = ' ';
+		dico_court = new File("monDicoCourt.txt");
+		dico_long = new File("monDicoLong.txt");
+		mots = new ArrayList<String>();
+
+	}
+
+	public void stockage1() throws IOException {
 		BufferedReader reader = new BufferedReader(new FileReader(dico_court));
-		ArrayList<String> mots = new ArrayList<String>();
-		
-		String test = reader.readLine();
 		//lecture du fichier dans ma liste
+		String test = reader.readLine();
 		while(test != null) {
 			mots.add(test); //on remplit notre liste des éléments du fichier "monDicoCourt.txt"
 			test = reader.readLine();
 		}
 
-		//création du tableau ou je stocke ma liste de mot.
+		//création du tableau ou je stocke ma liste de mots
 		String tab_mot [] = new String [mots.size()];
 		for(int i = 0; i < mots.size(); i++) {
 			tab_mot[i] = mots.get(i);
@@ -31,5 +37,33 @@ public class Jeu {
 
 		System.out.println(tab_mot[300]); //TEST1
 		System.out.println(tab_mot[264]); //TEST2
-	}
+	} 	
+
+	public void stockage2() throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(dico_long));
+		//lecture du fichier dans ma liste
+		String test = reader.readLine();
+		while(test != null) {
+			mots.add(test); //on remplit notre liste des éléments du fichier "monDicoCourt.txt"
+			test = reader.readLine();
+		}
+
+		//création du tableau ou je stocke ma liste de mots
+		String tab_mot [] = new String [mots.size()];
+		for(int i = 0; i < mots.size(); i++) {
+			tab_mot[i] = mots.get(i);
+		}
+
+		System.out.println(tab_mot[300]); //TEST1
+		System.out.println(tab_mot[264]); //TEST2
+	} 	
+	public static void main(String []args) throws IOException {	
+		Jeu parti1 = new Jeu();
+		Jeu parti2 = new Jeu();
+		parti1.stockage1();
+		parti2.stockage2();
+		/*ce programme affiche les mots aux indices 300 et 264.
+		de la liste de mot des documents texte "dicocourt" et "dicolong".
+		*/
+	}	
 }
