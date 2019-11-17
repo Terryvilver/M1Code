44e2330e341df25923107fc862043931db0035a4
Terry <terryvilver@gmail.com>
2019-11-17 23:04:02 +0100

    test du pendu, le but est de transmettre tous les mots du fichier texte a un tableau, qui aura un indice pour chaque mot souhaité

--- tp/projet/Jeu.java
+++ tp/projet/Jeu.java
@@ -7,22 +7,29 @@ import java.util.*;
 public class Jeu {
 	public int nb_essai;
 	public char lettre;
+	public File dico_court;
+	public BufferedReader reader;
+	public ArrayList<String> mots;
 	
 	public static void main(String []args) throws IOException {
-		File f = new File("monDicoCourt.txt");
-		BufferedReader reader = new BufferedReader(new FileReader(f));
-		ArrayList mots = new ArrayList();
-	
+		File dico_court = new File("monDicoCourt.txt");
+		BufferedReader reader = new BufferedReader(new FileReader(dico_court));
+		ArrayList<String> mots = new ArrayList<String>();
 		
-		String line = null;
-		
+		String test = reader.readLine();
 		//lecture du fichier dans ma liste
-		while((reader.readLine()) != null) {
-			mots.add(reader.readLine());
+		while(test != null) {
+			mots.add(test); //on remplit notre liste des éléments du fichier "monDicoCourt.txt"
+			test = reader.readLine();
 		}
 
-		if(mots.contains("adorer")) {
-			System.out.println("Le fichier contient le mot 'adorer'");
-		}	
+		//création du tableau ou je stocke ma liste de mot.
+		String tab_mot [] = new String [mots.size()];
+		for(int i = 0; i < mots.size(); i++) {
+			tab_mot[i] = mots.get(i);
+		}
+
+		System.out.println(tab_mot[300]); //TEST1
+		System.out.println(tab_mot[264]); //TEST2
 	}
 }
